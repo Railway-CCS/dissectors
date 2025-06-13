16365f6475d549fd7aeaa4b5cb4d7b7be4a18314
Markus Heinrich <markus.heinrich@incyde.com>
2025-06-13 15:16:24 +0200

    reduce bitwise operators to 32 bit

--- dissectors/rasta_modules/bit.lua
+++ dissectors/rasta_modules/bit.lua
@@ -11,26 +11,31 @@ set_plugin_info(my_info)
 
 local bit = nil
 
+print(_VERSION)
+
 if _VERSION == "Lua 5.2" then
+    print("A")
     local ok, e = pcall(require, "bit32")  -- Try bit32 (Lua 5.2)
     if ok then bit = e end
 elseif _VERSION == "Lua 5.1" then
+    print("B")
     local ok, e = pcall(require, "bit")  -- Try LuaJIT bit library
     if not ok then
         ok, e = pcall(require, "bit.numberlua")  -- Try numberlua for Lua 5.1
     end
     if ok then bit = e end
 else
+    print("C")
     -- Lua 5.3+ uses built-in bitwise operators
     bit = {
         band = function(a, b) return a & b end,
         bor = function(a, b) return a | b end,
         bxor = function(a, b) return a ~ b end,
         rshift = function(a, n) return a >> n end,
-        lshift = function(a, n) return a << n end,
+        lshift = function(a, n) return (a << n) & 0xffffffff end,
         bnot = function(a) return ~a end,
-		lrotate = function(a, n) return (a << n) | (a >> (32 - n)) end,
-		rrotate = function(a, n) return (a >> n) | (a << (32 - n)) end
+        lrotate = function(a, n) return ((a << n) & 0xffffffff) | (a >> (32 - n)) end,
+        rrotate = function(a, n) return (a >> n) | ((a << (32 - n)) & 0xffffffff) end
     }
 end
 
